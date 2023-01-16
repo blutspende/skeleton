845c4d1d7e550f58d273b7b1069bdf6dc19af8d8
Zsolt Nagy <nagzso14@gmail.com>
2023-01-16 22:19:20 +0100

    Fix several query.

--- repository_instrument.go
+++ repository_instrument.go
@@ -517,9 +517,9 @@ func (r *instrumentRepository) GetAnalyteMappings(ctx context.Context, instrumen
 }
 
 func (r *instrumentRepository) UpdateAnalyteMapping(ctx context.Context, analyteMapping AnalyteMapping) error {
-	query := fmt.Sprintf(`UPDATE %s.sk_analyte_mappings SET instrument_analyte = :instrument_analyte, analyte_id = :analyte_id, modified_at = timezone('utc', now()) WHERE id = :id`, r.dbSchema)
+	query := fmt.Sprintf(`UPDATE %s.sk_analyte_mappings SET instrument_analyte = :instrument_analyte, analyte_id = :analyte_id, modified_at = timezone('utc', now()) WHERE id = :id;`, r.dbSchema)
 	dao := convertAnalyteMappingToDAO(analyteMapping, uuid.Nil)
-	_, err := r.db.ExecContext(ctx, query, dao)
+	_, err := r.db.NamedExecContext(ctx, query, dao)
 	if err != nil {
 		log.Error().Err(err).Msg(msgUpdateAnalyteMappingFailed)
 		return ErrUpdateAnalyteMappingFailed
@@ -594,7 +594,7 @@ func (r *instrumentRepository) GetChannelMappings(ctx context.Context, analyteMa
 func (r *instrumentRepository) UpdateChannelMapping(ctx context.Context, channelMapping ChannelMapping) error {
 	query := fmt.Sprintf(`UPDATE %s.sk_channel_mappings SET instrument_channel = :instrument_channel, channel_id = :channel_id, modified_at = timezone('utc', now()) WHERE id = :id;`, r.dbSchema)
 	dao := convertChannelMappingToDAO(channelMapping, uuid.Nil)
-	_, err := r.db.ExecContext(ctx, query, dao)
+	_, err := r.db.NamedExecContext(ctx, query, dao)
 	if err != nil {
 		log.Error().Err(err).Msg(msgUpdateChannelMappingFailed)
 		return ErrUpdateChannelMappingFailed
@@ -664,7 +664,7 @@ func (r *instrumentRepository) GetResultMappings(ctx context.Context, analyteMap
 func (r *instrumentRepository) UpdateResultMapping(ctx context.Context, resultMapping ResultMapping) error {
 	query := fmt.Sprintf(`UPDATE %s.sk_result_mappings SET "key" = :key, "value" = :value, "index" = :index, modified_at = timezone('utc', now()) WHERE id = :id`, r.dbSchema)
 	dao := convertResultMappingToDAO(resultMapping, uuid.Nil)
-	_, err := r.db.ExecContext(ctx, query, dao)
+	_, err := r.db.NamedExecContext(ctx, query, dao)
 	if err != nil {
 		log.Error().Err(err).Msg(msgUpdateResultMappingFailed)
 		return ErrUpdateResultMappingFailed
@@ -736,7 +736,7 @@ func (r *instrumentRepository) UpsertRequestMappingAnalytes(ctx context.Context,
 func (r *instrumentRepository) UpdateRequestMapping(ctx context.Context, requestMapping RequestMapping) error {
 	query := fmt.Sprintf(`UPDATE %s.sk_result_mappings SET code = :code, modified_at = timezone('utc', now()) WHERE id = :id;`, r.dbSchema)
 	dao := convertRequestMappingToDAO(requestMapping, uuid.Nil)
-	_, err := r.db.ExecContext(ctx, query, dao)
+	_, err := r.db.NamedExecContext(ctx, query, dao)
 	if err != nil {
 		log.Error().Err(err).Msg(msgUpdateRequestMappingFailed)
 		return ErrUpdateRequestMappingFailed
