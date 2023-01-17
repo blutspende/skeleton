81d272cd53ab18995707f101ddba700bf1ebeaa3
Zsolt Nagy <nagzso14@gmail.com>
2023-01-17 17:56:10 +0100

    Fix save analysis request in batch query.

--- repository_analysis.go
+++ repository_analysis.go
@@ -175,24 +175,10 @@ func (r *analysisRepository) CreateAnalysisRequestsBatch(ctx context.Context, an
 	query := fmt.Sprintf(`INSERT INTO %s.sk_analysis_requests(id, work_item_id, analyte_id, sample_code, material_id, laboratory_id, valid_until_time)
 				VALUES(:id, :work_item_id, :analyte_id, :sample_code, :material_id, :laboratory_id, :valid_until_time) 
 				ON CONFLICT (work_item_id) DO 
-				UPDATE SET analyte_id = :analyte_id, sample_code = :sample_code, material_id = :material_id, laboratory_id = :laboratory_id, valid_until_time = :valid_until_time;`, r.dbSchema)
-
-	log.Trace().Int("batchSize", 500).Int("totalCount", len(analysisRequests)).
-		Msg("Saving analysis requests in batches")
-
-	var err error
-	partition(len(analysisRequests), 500, func(startIndex int, endIndex int) {
-		partition := analysisRequests[startIndex:endIndex]
-		if len(partition) < 1 {
-			return
-		}
-		_, err = r.db.NamedExecContext(ctx, query, convertAnalysisRequestsToDAOs(partition))
-		if err != nil {
-			log.Error().Err(err).Msg("Can not create RequestData")
-			return
-		}
-	})
+				UPDATE SET analyte_id = excluded.analyte_id, sample_code = excluded.sample_code, material_id = excluded.material_id, laboratory_id = excluded.laboratory_id, valid_until_time = excluded.valid_until_time;`, r.dbSchema)
+	_, err := r.db.NamedExecContext(ctx, query, convertAnalysisRequestsToDAOs(analysisRequests))
 	if err != nil {
+		log.Error().Err(err).Msg("Can not create RequestData")
 		return []uuid.UUID{}, nil
 	}
 
