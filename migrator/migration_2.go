517eec2a2ea4e864be847278b99afdbec6e5f4b8
Attila Rickert <attila.rickert@z-consulting.eu>
2023-03-09 16:12:25 +0100

    fix migration

--- migrator/migration_2.go
+++ migrator/migration_2.go
@@ -8,7 +8,7 @@ ALTER TABLE <SCHEMA_PLACEHOLDER>.sk_analysis_requests
 ADD COLUMN sent_to_instrument_count int not null default(0);
 
 CREATE TABLE <SCHEMA_PLACEHOLDER>.sk_analysis_request_instrument_transmissions(
-    id uuid not null default(now()),
+    id uuid not null default(uuid_generate_v4()),
     analysis_request_id uuid not null,
     instrument_id uuid not null,
     created_at timestamp not null default(now()),
