5ac73dc23164abebd8ecd1a2b983a0added5b9b6
Attila Rickert <attila.rickert@z-consulting.eu>
2023-09-12 09:20:34 +0200

    update instruments running modes from TEST/VALIDATION to SIMULATION/QUALIFICATION

--- migrator/migration_9.go
+++ migrator/migration_9.go
@@ -13,5 +13,13 @@ UPDATE <SCHEMA_PLACEHOLDER>.sk_analysis_results
 SET result_mode = 'QUALIFICATION'
 WHERE result_mode = 'VALIDATION';
 
+UPDATE <SCHEMA_PLACEHOLDER>.sk_instruments
+SET running_mode = 'SIMULATION'
+WHERE running_mode = 'TEST'
+
+UPDATE <SCHEMA_PLACEHOLDER>.sk_instruments
+SET running_mode = 'QUALIFICATION'
+WHERE running_mode = 'VALIDATION'
+
 DELETE FROM <SCHEMA_PLACEHOLDER>.sk_result_mode_definitions
 WHERE result_mode IN ('TEST','VALIDATION');`
