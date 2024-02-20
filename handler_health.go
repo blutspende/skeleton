bbb0e4a945a1a51fbecd38f20b7707c05ff4ce45
Attila Rickert <attila.rickert@z-consulting.eu>
2024-02-20 10:18:15 +0100

    extend healthcheck with memstats and build version

--- handler_health.go
+++ handler_health.go
@@ -1,28 +1,50 @@
 package skeleton
 
 import (
+	"fmt"
 	"net/http"
+	"runtime"
 
 	"github.com/gin-gonic/gin"
 )
 
-type buildInfoStruct struct {
-	BuildId       string `json:"buildId"`
-	BuildNr       string `json:"buildNr"`
-	SourceVersion string `json:"sourceVersion"`
-}
+// BuildVersion - will be filled at build process in pipeline
+var BuildVersion string
 
 type healthCheck struct {
-	Service    string   `json:"service"`
-	Status     string   `json:"status"`
-	ApiVersion []string `json:"apiVersion"`
+	Service      string   `json:"service"`
+	Status       string   `json:"status"`
+	ApiVersion   []string `json:"apiVersion"`
+	BuildVersion string   `json:"buildVersion"` // Docker build version
+	MemStats     memStats `json:"memStats"`
+}
+
+type memStats struct {
+	Alloc      string `json:"alloc"`
+	TotalAlloc string `json:"totalAlloc"`
+	Sys        string `json:"sys"`
+	HeapInUse  string `json:"heapInUse"`
+	HeapAlloc  string `json:"headAlloc"`
+	StackInUse string `json:"stackInUse"`
 }
 
 func (api *api) GetHealth(c *gin.Context) {
 	defaultInfo := healthCheck{
-		Service: "astm",
-		Status:  "running",
+		Service:      "astm",
+		Status:       "running",
+		ApiVersion:   []string{"v1"},
+		BuildVersion: BuildVersion,
 	}
+
+	var memStat runtime.MemStats
+	runtime.ReadMemStats(&memStat)
+
+	defaultInfo.MemStats.Alloc = fmt.Sprintf("%v MiB", memStat.Alloc/1024/1024)
+	defaultInfo.MemStats.TotalAlloc = fmt.Sprintf("%v MiB", memStat.TotalAlloc/1024/1024)
+	defaultInfo.MemStats.Sys = fmt.Sprintf("%v MiB", memStat.Sys/1024/1024)
+	defaultInfo.MemStats.HeapInUse = fmt.Sprintf("%v MiB", memStat.HeapInuse/1024/1024)
+	defaultInfo.MemStats.HeapAlloc = fmt.Sprintf("%v MiB", memStat.HeapAlloc/1024/1024)
+	defaultInfo.MemStats.StackInUse = fmt.Sprintf("%v MiB", memStat.StackInuse/1024/1024)
 
 	c.JSON(http.StatusOK, defaultInfo)
 }
