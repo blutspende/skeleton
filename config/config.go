a834aea72d523609d7bcd77a1a2a9ad959fe41be
Mihály Neumann <neumannmisi@gmail.com>
2026-06-12 10:03:24 +0200

    extend config, update dependencies

--- config/config.go
+++ config/config.go
@@ -27,61 +27,63 @@ type Configuration struct {
 		Database string `envconfig:"DB_DATABASE" required:"true"`
 		SSLMode  string `envconfig:"DB_SSL_MODE" required:"true"`
 	}
-	APIPort                                    uint16        `envconfig:"API_PORT" default:"8080"`
-	Authorization                              bool          `envconfig:"AUTHORIZATION" default:"true"`
-	ClientID                                   string        `envconfig:"CLIENT_ID" required:"true"`
-	ClientSecret                               string        `envconfig:"CLIENT_SECRET" required:"true"`
-	EnableTLS                                  bool          `envconfig:"ENABLE_TLS" default:"false"`
-	BloodlabCertPath                           string        `envconfig:"BLOODLAB_CERT_PATH" default:"../bloodlab_cert.pem"`
-	BloodlabKeyPath                            string        `envconfig:"BLOODLAB_KEY_PATH" default:"../bloodlab_key.pem"`
-	Development                                bool          `envconfig:"DEVELOPMENT" default:"false"`
-	PermittedOrigin                            string        `envconfig:"PERMITTED_ORIGIN_URL" default:"*"`
-	OIDCBaseURL                                string        `envconfig:"OIDC_BASE_URL" default:"https://iam.bloodlab.org/realms/test.bloodlab.org"`
-	LogLevel                                   zerolog.Level `envconfig:"LOG_LEVEL" default:"1"`
-	ApplicationName                            string        `envconfig:"APPLICATION_NAME" default:"astm"`
-	TCPListenerPort                            int           `envconfig:"TCP_LISTENER_PORT" required:"true" default:"5000"`
-	LocalEnvironment                           bool          `envconfig:"LOCAL_ENVIRONMENT" default:"false"`
-	InstrumentTransferRetryDelayInMs           int           `envconfig:"INSTRUMENT_TRANSFER_DELAY" default:"600000"`
-	CerberusURL                                string        `envconfig:"CERBERUS_URL" required:"true" default:"http://cerberus"`
-	DeaURL                                     string        `envconfig:"DEA_URL" required:"true" default:"http://dea"`
-	LookBackDays                               int           `envconfig:"LOOK_BACK_DAYS" default:"14"`
-	TCPServerMaxConnections                    int           `envconfig:"MAX_TCP_CONNECTIONS" required:"true" default:"50"`
-	Proxy                                      string        `envconfig:"PROXY" default:""`
-	RequestTransferSleepTimer                  int           `envconfig:"REQUEST_TRANSFER_SLEEP_TIME" default:"500"`
-	ResultTransferBatchSize                    int           `envconfig:"RESULT_TRANSFER_BATCH_SIZE" default:"100"`
-	ResultTransferFlushTimeout                 int           `envconfig:"RESULT_TRANSFER_FLUSH_TIMEOUT" default:"5"`
-	ResultTransferRetryTimeout                 int           `envconfig:"RESULT_TRANSFER_RETRY_TIMEOUT" default:"5"`
-	ResultBufferFlushTimeout                   int           `envconfig:"RESULT_BUFFER_FLUSH_TIMEOUT" default:"3"`
-	CerberusQueueItemRetryTimeout              int           `envconfig:"CERBERUS_QUEUE_ITEM_RETRY_TIMEOUT" default:"10"`
-	ImageRetrySeconds                          int           `envconfig:"IMAGE_RETRY_SECONDS" default:"60"`
-	AnalysisRequestsChannelBufferSize          int           `envconfig:"ANALYSIS_REQUESTS_CHANNEL_BUFFER_SIZE" default:"1024"`
-	AnalysisRequestWorkerPoolSize              int           `envconfig:"ANALYSIS_REQUEST_WORKER_POOL_SIZE" default:"3"`
-	InstrumentDriverRegistrationTimeoutSeconds int           `envconfig:"INSTRUMENT_DRIVER_REGISTRATION_RETRY_TIMEOUT" default:"10"`
-	InstrumentDriverRegistrationMaxRetry       int           `envconfig:"INSTRUMENT_DRIVER_REGISTRATION_MAX_RETRY" default:"20"`
-	CleanupDays                                int           `envconfig:"CLEANUP_DAYS" default:"90"`
-	CleanupJobRunIntervalHours                 int           `envconfig:"CLEANUP_JOB_RUN_INTERVAL_HOURS" default:"4"`
-	UnprocessedAnalysisRequestErrorRetryMinute int           `envconfig:"UNPROCESSED_ANALYSIS_REQUEST_ERROR_RETRY" default:"5"`
-	UnprocessedAnalysisResultErrorRetryMinute  int           `envconfig:"UNPROCESSED_ANALYSIS_RESULT_ERROR_RETRY" default:"5"`
-	GetUnprocessedAnalysisRequestRetryMinute   int           `envconfig:"GET_UNPROCESSED_ANALYSIS_REQUEST_RETRY" default:"5"`
-	GetUnprocessedAnalysisResultIDsRetryMinute int           `envconfig:"GET_UNPROCESSED_ANALYSIS_RESULT_IDS_RETRY" default:"5"`
-	GetUnprocessedControlResultIDsRetryMinute  int           `envconfig:"GET_UNPROCESSED_CONTROL_RESULT_IDS_RETRY" default:"5"`
-	StandardAPIClientTimeoutSeconds            uint          `envconfig:"STANDARD_API_CLIENT_TIMEOUT_SECONDS" default:"10"`
-	LongPollingAPIClientTimeoutSeconds         uint          `envconfig:"LONG_POLLING_API_CLIENT_TIMEOUT_SECONDS" default:"80"`
-	LongPollingReattemptWaitSeconds            uint          `envconfig:"LONG_POLLING_REATTEMPT_WAIT_SECONDS" default:"5"`
-	LongPollingLoggingEnabled                  bool          `envconfig:"LONG_POLLING_LOGGING_ENABLED" default:"false"`
-	LongPollingRetrySeconds                    int           `envconfig:"LONG_POLLING_RETRY_SECONDS" default:"30"`
-	MessageMaxRetries                          int           `envconfig:"MESSAGE_MAX_RETRIES" default:"25"`
-	MessageSampleCodeMaxRetries                int           `envconfig:"MESSAGE_SAMPLE_CODE_MAX_RETRIES" default:"25"`
-	BlackListedTCPClientIPAddresses            string        `envconfig:"BLACKLISTED_TCP_CLIENT_IP_ADDRESSES" default:""`
-	MaxRequestsPerSecond                       rate.Limit    `envconfig:"MAX_REQUESTS_PER_SECOND" default:"15"`
-	AnalysisResultWithoutControlSearchDays     int           `envconfig:"ANALYSIS_RESULT_WITHOUT_CONTROL_SEARCH_DAYS" default:"14"`
-	AnalysisResultWithInvalidControlSearchDays int           `envconfig:"ANALYSIS_RESULT_WITH_INVALID_CONTROL_SEARCH_DAYS" default:"14"`
-	ControlResultSearchDays                    int           `envconfig:"CONTROL_RESULT_SEARCH_DAYS" default:"14"`
-	SampleSeenMessageFlushSeconds              int           `envconfig:"SAMPLE_SEEN_MESSAGE_FLUSH_SECONDS" default:"30"`
-	ConsoleLogFlushSeconds                     int           `envconfig:"CONSOLE_LOG_FLUSH_SECONDS" default:"30"`
-	SampleSeenBatchSize                        int           `envconfig:"SAMPLE_SEEN_BATCH_SIZE" default:"100"`
-	SampleSeenBatchTimeOut                     int           `envconfig:"SAMPLE_SEEN_BATCH_TIMEOUT" default:"5"`
-	ClientCredentialAuthHeaderValue            string
+	APIPort                                     uint16        `envconfig:"API_PORT" default:"8080"`
+	Authorization                               bool          `envconfig:"AUTHORIZATION" default:"true"`
+	ClientID                                    string        `envconfig:"CLIENT_ID" required:"true"`
+	ClientSecret                                string        `envconfig:"CLIENT_SECRET" required:"true"`
+	EnableTLS                                   bool          `envconfig:"ENABLE_TLS" default:"false"`
+	BloodlabCertPath                            string        `envconfig:"BLOODLAB_CERT_PATH" default:"../bloodlab_cert.pem"`
+	BloodlabKeyPath                             string        `envconfig:"BLOODLAB_KEY_PATH" default:"../bloodlab_key.pem"`
+	Development                                 bool          `envconfig:"DEVELOPMENT" default:"false"`
+	PermittedOrigin                             string        `envconfig:"PERMITTED_ORIGIN_URL" default:"*"`
+	OIDCBaseURL                                 string        `envconfig:"OIDC_BASE_URL" default:"https://iam.bloodlab.org/realms/test.bloodlab.org"`
+	LogLevel                                    zerolog.Level `envconfig:"LOG_LEVEL" default:"1"`
+	ApplicationName                             string        `envconfig:"APPLICATION_NAME" default:"astm"`
+	TCPListenerPort                             int           `envconfig:"TCP_LISTENER_PORT" required:"true" default:"5000"`
+	LocalEnvironment                            bool          `envconfig:"LOCAL_ENVIRONMENT" default:"false"`
+	InstrumentTransferRetryDelayInMs            int           `envconfig:"INSTRUMENT_TRANSFER_DELAY" default:"600000"`
+	CerberusURL                                 string        `envconfig:"CERBERUS_URL" required:"true" default:"http://cerberus"`
+	DeaURL                                      string        `envconfig:"DEA_URL" required:"true" default:"http://dea"`
+	LookBackDays                                int           `envconfig:"LOOK_BACK_DAYS" default:"14"`
+	TCPServerMaxConnections                     int           `envconfig:"MAX_TCP_CONNECTIONS" required:"true" default:"50"`
+	Proxy                                       string        `envconfig:"PROXY" default:""`
+	RequestTransferSleepTimer                   int           `envconfig:"REQUEST_TRANSFER_SLEEP_TIME" default:"500"`
+	ResultTransferBatchSize                     int           `envconfig:"RESULT_TRANSFER_BATCH_SIZE" default:"100"`
+	ResultTransferFlushTimeout                  int           `envconfig:"RESULT_TRANSFER_FLUSH_TIMEOUT" default:"5"`
+	ResultTransferRetryTimeout                  int           `envconfig:"RESULT_TRANSFER_RETRY_TIMEOUT" default:"5"`
+	ResultBufferFlushTimeout                    int           `envconfig:"RESULT_BUFFER_FLUSH_TIMEOUT" default:"3"`
+	CerberusQueueItemRetryTimeout               int           `envconfig:"CERBERUS_QUEUE_ITEM_RETRY_TIMEOUT" default:"10"`
+	ImageRetrySeconds                           int           `envconfig:"IMAGE_RETRY_SECONDS" default:"60"`
+	AnalysisRequestsChannelBufferSize           int           `envconfig:"ANALYSIS_REQUESTS_CHANNEL_BUFFER_SIZE" default:"1024"`
+	AnalysisRequestWorkerPoolSize               int           `envconfig:"ANALYSIS_REQUEST_WORKER_POOL_SIZE" default:"3"`
+	InstrumentDriverRegistrationTimeoutSeconds  int           `envconfig:"INSTRUMENT_DRIVER_REGISTRATION_RETRY_TIMEOUT" default:"10"`
+	InstrumentDriverRegistrationMaxRetry        int           `envconfig:"INSTRUMENT_DRIVER_REGISTRATION_MAX_RETRY" default:"20"`
+	CleanupDays                                 int           `envconfig:"CLEANUP_DAYS" default:"90"`
+	CleanupJobRunIntervalHours                  int           `envconfig:"CLEANUP_JOB_RUN_INTERVAL_HOURS" default:"4"`
+	UnprocessedAnalysisRequestErrorRetryMinute  int           `envconfig:"UNPROCESSED_ANALYSIS_REQUEST_ERROR_RETRY" default:"5"`
+	UnprocessedAnalysisResultErrorRetryMinute   int           `envconfig:"UNPROCESSED_ANALYSIS_RESULT_ERROR_RETRY" default:"5"`
+	GetUnprocessedAnalysisRequestRetryMinute    int           `envconfig:"GET_UNPROCESSED_ANALYSIS_REQUEST_RETRY" default:"5"`
+	GetUnprocessedAnalysisResultIDsRetryMinute  int           `envconfig:"GET_UNPROCESSED_ANALYSIS_RESULT_IDS_RETRY" default:"5"`
+	GetUnprocessedControlResultIDsRetryMinute   int           `envconfig:"GET_UNPROCESSED_CONTROL_RESULT_IDS_RETRY" default:"5"`
+	StandardAPIClientTimeoutSeconds             uint          `envconfig:"STANDARD_API_CLIENT_TIMEOUT_SECONDS" default:"10"`
+	LongPollingAPIClientTimeoutSeconds          uint          `envconfig:"LONG_POLLING_API_CLIENT_TIMEOUT_SECONDS" default:"80"`
+	LongPollingReattemptWaitSeconds             uint          `envconfig:"LONG_POLLING_REATTEMPT_WAIT_SECONDS" default:"5"`
+	LongPollingLoggingEnabled                   bool          `envconfig:"LONG_POLLING_LOGGING_ENABLED" default:"false"`
+	LongPollingRetrySeconds                     int           `envconfig:"LONG_POLLING_RETRY_SECONDS" default:"30"`
+	MessageRetryTimeoutSeconds                  int           `envconfig:"MESSAGE_RETRY_TIMEOUT_SECONDS" default:"30"`
+	MessageMaxRetries                           int           `envconfig:"MESSAGE_MAX_RETRIES" default:"25"`
+	MessageSampleCodeMaxRetries                 int           `envconfig:"MESSAGE_SAMPLE_CODE_MAX_RETRIES" default:"25"`
+	BlackListedTCPClientIPAddresses             string        `envconfig:"BLACKLISTED_TCP_CLIENT_IP_ADDRESSES" default:""`
+	TCPServerConnectionInitiationTimeoutSeconds uint16        `envconfig:"TCP_CONNECTION_INITIATION_TIMEOUT_SECONDS" default:"30"`
+	MaxRequestsPerSecond                        rate.Limit    `envconfig:"MAX_REQUESTS_PER_SECOND" default:"15"`
+	AnalysisResultWithoutControlSearchDays      int           `envconfig:"ANALYSIS_RESULT_WITHOUT_CONTROL_SEARCH_DAYS" default:"14"`
+	AnalysisResultWithInvalidControlSearchDays  int           `envconfig:"ANALYSIS_RESULT_WITH_INVALID_CONTROL_SEARCH_DAYS" default:"14"`
+	ControlResultSearchDays                     int           `envconfig:"CONTROL_RESULT_SEARCH_DAYS" default:"14"`
+	SampleSeenMessageFlushSeconds               int           `envconfig:"SAMPLE_SEEN_MESSAGE_FLUSH_SECONDS" default:"30"`
+	ConsoleLogFlushSeconds                      int           `envconfig:"CONSOLE_LOG_FLUSH_SECONDS" default:"30"`
+	SampleSeenBatchSize                         int           `envconfig:"SAMPLE_SEEN_BATCH_SIZE" default:"100"`
+	SampleSeenBatchTimeOut                      int           `envconfig:"SAMPLE_SEEN_BATCH_TIMEOUT" default:"5"`
+	ClientCredentialAuthHeaderValue             string
 }
 
 var Settings Configuration
